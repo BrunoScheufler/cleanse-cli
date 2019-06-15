03f0d8dbcf0a00911c4f1e3a59c232050eb8d7b3
Bruno Scheufler <signing@brunoscheufler.com>
2019-06-15 18:12:13 +0200

    style(code): add renovate config and update styling

--- src/index.ts
+++ src/index.ts
@@ -8,40 +8,40 @@ import chalk from 'chalk';
 const spinner = ora();
 
 const removePath = async (path: string) => {
-	await remove(path);
+  await remove(path);
 
-	if (await pathExists(path)) {
-		throw new Error(`Could not remove ${path}`);
-	}
+  if (await pathExists(path)) {
+    throw new Error(`Could not remove ${path}`);
+  }
 };
 
 const handleError = (err: Error) => {
-	spinner.fail(chalk.redBright(err.message));
-	process.exit(1);
+  spinner.fail(chalk.redBright(err.message));
+  process.exit(1);
 };
 
 (async () => {
-	process.on('uncaughtException', handleError);
-	process.on('unhandledRejection', handleError);
+  process.on('uncaughtException', handleError);
+  process.on('unhandledRejection', handleError);
 
-	const paths = await globby(['**/node_modules', '!**/node_modules/**/node_modules'], { expandDirectories: false, onlyDirectories: true });
+  const paths = await globby(['**/node_modules', '!**/node_modules/**/node_modules'], { expandDirectories: false, onlyDirectories: true });
 
-	try {
-		spinner.start(`Starting to cleanse ${paths.length} entries`);
+  try {
+    spinner.start(`Starting to cleanse ${paths.length} entries`);
 
-		for (let i = 0; i < paths.length; i++) {
-			const path = paths[i];
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
 
-			const progressText = `${i + 1}/${paths.length}`;
-			spinner.text = `(${chalk.greenBright(progressText)}) Cleansing ${chalk.gray(path)}`;
-			spinner.render();
+      const progressText = `${i + 1}/${paths.length}`;
+      spinner.text = `(${chalk.greenBright(progressText)}) Cleansing ${chalk.gray(path)}`;
+      spinner.render();
 
-			await removePath(path);
-		}
+      await removePath(path);
+    }
 
-		const entryText = paths.length !== 1 ? 'entries' : 'entry';
-		spinner.succeed(`Successfully cleansed ${paths.length} ${entryText}!`);
-	} catch (err) {
-		handleError(err);
-	}
+    const entryText = paths.length !== 1 ? 'entries' : 'entry';
+    spinner.succeed(`Successfully cleansed ${paths.length} ${entryText}!`);
+  } catch (err) {
+    handleError(err);
+  }
 })();
